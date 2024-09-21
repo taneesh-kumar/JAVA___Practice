a11f5ab3440c5cc2623fdcc4e24c8c9ee9cf3da1
taneesh-kumar <heyitstaneesh@gmail.com>
2024-09-21 19:34:26 +0530

    Day-2

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Both numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Choose Operarion");
+        System.out.println("Add enter[1]");
+        System.out.println("Sub enter[2]");
+        System.out.println("mul enter[3]");
+        System.out.println("div enter[4]");
+        int n = sc.nextInt();
+        switch (n) {
+            case 1: System.out.println(a + b);
+            break;
+            case 2: System.out.println(a - b);
+            break;
+            case 3: System.out.println(a * b);
+            break;
+            case 4: if(b == 0){
+                System.out.println("Calculation Undefined");
+            }else{
+                System.out.println(a / b);
+            }
+            break;
+        }
+        
+    }
+}
