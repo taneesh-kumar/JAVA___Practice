29a7f4f397cd8e300adea86b40b0f0e1943ac126
taneesh-kumar <heyitstaneesh@gmail.com>
2024-09-25 20:19:24 +0530

    day-3, APP-4

---
+++ AdvPatternProb4.java
@@ -0,0 +1,23 @@
+//    1
+//   212
+//  32123
+// 4321234
+//543212345
+
+public class AdvPatternProb4 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            for(int j=n-i; j>=1; j--){
+                System.out.print("  ");
+            }
+            for(int j=i; j>=1; j--){
+                System.out.print(j+" ");
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
