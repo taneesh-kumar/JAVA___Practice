807247e5f9f342e9a277589be54104dd056dd338
taneesh-kumar <heyitstaneesh@gmail.com>
2024-10-03 09:17:23 +0530

    Day-5

--- AscendingArray.java
+++ AscendingArray.java
@@ -1,5 +1,32 @@
 import java.util.*;
 
 public class AscendingArray {
-Scanner sc = new Scanner(System.in);    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array size");
+        int size = sc.nextInt();
+        int [] num = new int[size];
+
+        System.out.println("array entry");
+        for(int i=0; i<size; i++){
+            num[i] = sc.nextInt();
+        }
+
+        boolean IsAscending = true;
+        
+        for(int i=0; i<size-1; i++){
+            if (num[i] > num[i+1]) {
+                IsAscending = false;
+            }
+        }
+
+        if (IsAscending == true) {
+            System.out.println("Array is Ascending");
+        } 
+        else{
+            System.out.println("Array is not Ascending");
+        }
+
+
+    }
 }
