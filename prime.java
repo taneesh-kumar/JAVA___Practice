40e596bccb758c401a38292f2ca64dc514da11b7
taneesh-kumar <heyitstaneesh@gmail.com>
2024-10-24 09:45:15 +0530

    Optimised version

--- prime.java
+++ prime.java
@@ -1,28 +1,34 @@
 import java.util.*;
 
-public class prime {
+public class main {
+    public static boolean IsPrime(int num){
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2 || num == 3) {
+            return true;
+        }
+        if (num%2 == 0 || num%3 == 0) {
+            return false;
+        }
+        for(int i=5; i*i<=num; i+=6){
+            if (num%i == 0 || num%(i+2) == 0 ) {
+                return false;
+            }
+        }
+        return true;
+    }    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int input;
-        do{
-        System.out.println("Enter number");
+        System.out.println("Enter your number");
         int n = sc.nextInt();
-        int div = 0;
-        for (int i=2; i<n; i++){
-           if(n % i == 0){
-            div++;
-           }
+
+        if (IsPrime(n)) {
+            System.out.println(n+" ia a prime");
         }
-        if (n == 1){
-            System.out.println("1 is neither prime nor composit");
-        } else if(div == 0){
-            System.out.println(n+" is prime");
-        } else {
+        else{
             System.out.println(n+" is not prime");
         }
-        System.out.println("want to continue? yes[1] no[0]");
-        input = sc.nextInt();
-    }while(input == 1);
     }
 }
