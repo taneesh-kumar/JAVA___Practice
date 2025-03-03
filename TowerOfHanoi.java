770ba2b5c5b949a21b2f13d9f97942956b36f1cf
Taneesh <taneesh@localhost.localdomain>
2025-03-03 13:33:41 +0530

    conf

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,14 +1,14 @@
 public class TowerOfHanoi {
-    public static void solveHanoi(int n, char source, char auxiliary, char destination) {
+    public static void solveHanoi(int n, char source, char support, char destination) {
         if (n == 1) {
             System.out.println("Move disk 1 from " + source + " to " + destination);
             return;
         }
-        solveHanoi(n - 1, source, destination, auxiliary);
+        solveHanoi(n - 1, source, destination, support);
         
         System.out.println("Move disk " + n + " from " + source + " to " + destination);
         
-        solveHanoi(n - 1, auxiliary, source, destination);
+        solveHanoi(n - 1, support, source, destination);
     }
 
     public static void main(String[] args) {
