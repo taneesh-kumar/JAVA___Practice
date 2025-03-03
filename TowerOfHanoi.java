bbf0d44ae5839f82a860c19dcce5c6b4e59e509c
taneesh-kumar <taneeshvalupadasu@gmail.com>
2025-03-03 18:30:40 +0530

    recur

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,18 +1,17 @@
 public class TowerOfHanoi {
-    public static void solveHanoi(int n, char source, char support, char destination) {
+
+    public static void solveHanoi(int n, char source, char helper, char destination){
         if (n == 1) {
-            System.out.println("Move disk 1 from " + source + " to " + destination);
+            System.out.println("Move element 1 from "+source+" to "+destination);
             return;
         }
-        solveHanoi(n - 1, source, destination, support);
-        
-        System.out.println("Move disk " + n + " from " + source + " to " + destination);
-        
-        solveHanoi(n - 1, support, source, destination);
+        solveHanoi(n-1, source, destination, helper);
+        System.out.println("Move element "+n+" from "+source+" to "+destination);
+        solveHanoi(n-1, helper, source, destination);
     }
 
     public static void main(String[] args) {
-        int n = 3;
-        solveHanoi(n, 'A', 'B', 'C'); 
+        int number = 2;
+        solveHanoi(number, 'A', 'B', 'C');
     }
 }
