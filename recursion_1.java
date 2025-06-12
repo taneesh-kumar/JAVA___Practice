8e1923e80a50377f1c7e50e67817c941f55c67aa
taneesh-kumar <heyitstaneesh@gmail.com>
2025-06-12 19:42:29 +0530

    s

--- recursion_1.java
+++ recursion_1.java
@@ -1,16 +1,3 @@
-public class recursion_1 {
-
-    public static void PrintNum(int n){
-        if (n > 10){
-            return;
-        }
-        System.out.println(n);
-        PrintNum(n+1);
-    }
-
-    public static void main(String[] args) {
-        int num = 1;
-
-        PrintNum(num);
-    }
+public class Recursion_1 {
+    
 }
